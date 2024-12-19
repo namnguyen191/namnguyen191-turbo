d89c8e1a467c5386032629f968649c140fa47377
Nam Nguyen <hoangnamnguyen191@gmail.com>
2024-12-18 21:11:29 -0500

    feat: remove allowSignalWrite as it is deprecated from angular lib

--- angular-workspace/projects/common-angular-helper/src/lib/hooks/computed-from-observable.ts
+++ angular-workspace/projects/common-angular-helper/src/lib/hooks/computed-from-observable.ts
@@ -4,18 +4,15 @@ import { Observable } from 'rxjs';
 export const computedFromObservable = <T>(getObs: () => Observable<T>): Signal<T | undefined> => {
   const sigToReturn = signal<T | undefined>(undefined);
 
-  effect(
-    (onCleanup) => {
-      const obs = getObs();
+  effect((onCleanup) => {
+    const obs = getObs();
 
-      const sub = obs.subscribe((val) => sigToReturn.set(val));
+    const sub = obs.subscribe((val) => sigToReturn.set(val));
 
-      onCleanup(() => {
-        sub.unsubscribe();
-      });
-    },
-    { allowSignalWrites: true }
-  );
+    onCleanup(() => {
+      sub.unsubscribe();
+    });
+  });
 
   return sigToReturn;
 };
